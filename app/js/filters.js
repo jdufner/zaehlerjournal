b2e96adb5a1ca1e967c4d4ed0d10daf99e0b85a7
jdufner <juergen@jdufner.de>
2014-09-10 23:56:27 +0200

    Resolves #5

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,16 @@
 
 /* Filters */
 
-angular.module('zaehlerjournal.filters', []).
-  filter('interpolate', ['version', function(version) {
+angular.module('zaehlerjournal.filters', [])
+  .filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     };
-  }]);
+  }])
+  .filter('encodeUri', function() {
+    return window.encodeURI;
+  })
+  .filter('encodeUriComponent', function() {
+    return window.encodeURIComponent;
+  })
+;
