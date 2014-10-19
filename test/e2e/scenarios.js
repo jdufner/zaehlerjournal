6acfb5a652b040b47c44ef69f00c1d9b7885c194
jdufner <juergen@jdufner.de>
2014-10-20 01:32:58 +0200

    End2End-Test vorangetrieben

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -5,50 +5,62 @@
 describe('Zählerjournal', function() {
 
   browser.get('index.html');
-
-  it('should automatically redirect to /uebersichtt when location hash/fragment is empty', function() {
+  it('should automatically redirect to /uebersicht when location hash/fragment is empty', function() {
     expect(browser.getLocationAbsUrl()).toMatch("/uebersicht");
   });
 
-
   describe('uebersicht', function() {
-
     beforeEach(function() {
       browser.get('index.html#/uebersicht');
     });
-
     it('should render uebersicht when user navigates to /uebersicht', function() {
-      expect(element.all(by.css('[ng-view] h1')).first().getText()).
-        toMatch(/bersicht/);
+      expect(element.all(by.css('[ng-view] h1')).first().getText()).toMatch(/bersicht/);
     });
-
   });
 
-
-  describe('details', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/zaehler/04AB019104');
-    });
-
-    it('should render details when user navigates to /zaehler/04AB019104', function() {
-      expect(element.all(by.css('[ng-view] h1')).first().getText()).
-        toMatch(/04AB019104/);
-    });
-
-  });
-
-  describe('einstellungen', function() {
-
+  describe('Einstellungen aufrufen und eine Immobilie anlegen', function() {
     beforeEach(function() {
       browser.get('index.html#/einstellungen');
     });
-
     it('should render einstellungen when user navigates to /einstellungen', function() {
-      expect(element.all(by.css('[ng-view] h1')).first().getText()).
-        toMatch(/Einstellungen/);
+      expect(element.all(by.css('[ng-view] h1')).first().getText()).toMatch(/Einstellungen/);
     });
-
+    it('should create Immobilie', function() {
+      element(by.id('adresse')).sendKeys('Strasse Hausnr Ort');
+      element(by.buttonText('Speichern')).click();
+      var buttons = element.all(by.css('[ng-view] a.btn'));
+      expect(buttons.count()).toBe(1);
+      var button = buttons.first();
+      expect(button.getText()).toMatch(/Strasse Hausnr Ort/);
+      button.click().then(function(result) {
+        //console.log(result);
+      });
+      expect(element.all(by.css('[ng-view] h1')).first().getText()).toMatch(/Strasse Hausnr Ort/);
+    });
   });
 
+  describe('Immobilie aufrufen und einen Zaehler anlegen', function() {
+    beforeEach(function() {
+      browser.get('index.html#/einstellungen');
+      element(by.id('adresse')).sendKeys('Strasse Hausnr Ort');
+      element(by.buttonText('Speichern')).click();
+      element(by.css('[ng-view] a.btn')).click().then(function(result) {
+        //console.log(result);
+      });
+    });
+    it('should create zaehler', function() {
+      expect(element.all(by.css('[ng-view] h1')).first().getText()).toMatch(/Strasse Hausnr Ort/);
+      element(by.id('nr')).sendKeys('123');
+//      element(by.css('select#art option:contains("Gas")')).click();
+      element.all(by.css('select#art option')).then(function(options){
+        options[2].click();
+      });
+      element.all(by.css('select#typ option')).then(function(options){
+        options[1].click();
+      });
+      element(by.buttonText('Speichern')).click();
+      protractor.getInstance().sleep(5000);
+    });
+  });
+  
 });
